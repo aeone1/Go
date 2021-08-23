03e14b7f8ead008e228d6bba2e10ad57a35a5afc
andiemen-hookprod <ap@hookprod.com>
2021-08-23 21:28:25 +0300

    timing the traveler functions

--- go-grid-traveler/main.go
+++ go-grid-traveler/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 /*
 How many ways can I get from the top left
-to the bottom right. 
+to the bottom right.
 */
 
 func main() {
-	fmt.Printf("gridTraveler(1, 1) = %d\n", gridTraveler(1, 1)) //1
+	//defer timeTrack(time.Now(), "gridTraveler")
+	//fmt.Printf("gridTraveler(1, 1) = %d\n", gridTraveler(1, 1)) //1
 	//fmt.Printf("gridTraveler(3, 2) = %d\n", gridTraveler(3, 2)) //3
 	//fmt.Printf("gridTraveler(2, 3) = %d\n", gridTraveler(2, 3)) //3
 	//fmt.Printf("gridTraveler(3, 3) = %d\n", gridTraveler(3, 3)) //6
@@ -21,10 +23,13 @@ func main() {
 	//fmt.Printf("gridTravelerMem(3, 2) = %d\n", gridTravelerMem(3, 2, make(map[string]int))) //3
 	//fmt.Printf("gridTravelerMem(2, 3) = %d\n", gridTravelerMem(2, 3, make(map[string]int))) //3
 	//fmt.Printf("gridTravelerMem(3, 3) = %d\n", gridTravelerMem(3, 3, make(map[string]int))) //6
+	defer timeTrack(time.Now(), "gridTravelerMem")
+
 	fmt.Printf("gridTravelerMem(18, 18) = %d\n", gridTravelerMem(18, 18, make(map[string]int))) //2333606220
 }
 
 func gridTraveler(m int, n int) int {
+	
 	if m == 1 && n == 1  {
 		return 1
 	}
@@ -37,16 +42,16 @@ func gridTraveler(m int, n int) int {
 func gridTravelerMem(m int, n int, memo map[string]int) int {
 	key := strconv.Itoa(m) + "," + strconv.Itoa(n)
 	keyReverse := strconv.Itoa(n) + "," + strconv.Itoa(m)
-	fmt.Printf("key: %s\n", key)
-	fmt.Printf("keyReverse: %s\n", keyReverse)
+	//fmt.Printf("key: %s\n", key)
+	//fmt.Printf("keyReverse: %s\n", keyReverse)
 
 	if val, ok := memo[key]; ok {
-		fmt.Printf("found key - val: %s - %d\n", key, val)
+		//fmt.Printf("found key - val: %s - %d\n", key, val)
 		return val
 	}
 
 	if val, ok := memo[keyReverse]; ok {
-		fmt.Printf("found keyReverse - val: %s - %d\n", keyReverse, val)
+		//fmt.Printf("found keyReverse - val: %s - %d\n", keyReverse, val)
 		return val
 	}
 
@@ -58,4 +63,9 @@ func gridTravelerMem(m int, n int, memo map[string]int) int {
 	}
 	memo[key] = gridTravelerMem(m-1, n, memo) + gridTravelerMem(m, n-1, memo)
 	return memo[key]
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s\n", name, elapsed)
 }
