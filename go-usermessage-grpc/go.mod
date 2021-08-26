20d3c28739dc39b194295653343ac637494abb19
andiemen-hookprod <ap@hookprod.com>
2021-08-26 15:40:54 +0300

    env, runs locally, +docker

--- go-usermessage-grpc/go.mod
+++ go-usermessage-grpc/go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.4.3
-	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.13.0
+	github.com/joho/godotenv v1.3.0
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.25.0
 )
--- go-usermessage-grpc/usermsg_server/usermsg_server.go
+++ go-usermessage-grpc/usermsg_server/usermsg_server.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
-	"io/ioutil"
+	"time"
 
 	pb "github.com/aeone1/Go/tree/master/go-usermessage-grpc/usermsg"
+	"github.com/jackc/pgx/v4"
 	"google.golang.org/grpc"
-	"github.com/google/uuid"
-	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"github.com/joho/godotenv"
 )
 
 /*
 TODO: Error handling func and error messages (grpcError)
 */
 
-const (
-	port = ":50515"
-	fileName = "user_message.json"
-)
 
 func NewUserMessageServer() *UserMessageServer {
 	return &UserMessageServer{
@@ -28,10 +26,11 @@ func NewUserMessageServer() *UserMessageServer {
 }
 type UserMessageServer struct {
 	pb.UnimplementedUserMessageServer
+	conn *pgx.Conn
 }
 
 func (server *UserMessageServer) Run() error {
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s",os.Getenv("GO_PORT")))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -42,76 +41,159 @@ func (server *UserMessageServer) Run() error {
 	return s.Serve(lis)
 }
 
-func WriteToFile (m *pb.MessageList, fileName string) (string, error) {
-	jsonBytes, err := protojson.Marshal(m)
-	if err != nil {
-		return "Json Marshaling failed: %v", err
-	}
-	if err := ioutil.WriteFile(fileName, jsonBytes, 0664); err != nil {
-		return "Failed write to file: %v", err
-	}
-	return "", nil
-}
 
-func (s *UserMessageServer) CreateNewMessage(
+func (server *UserMessageServer) CreateNewMessage(
 	ctx context.Context, 
 	in *pb.NewMessage,
 	) (*pb.Message, error) {
-	log.Printf("Recieved: %v", in)
-	// check file exist by reading it
-	readBytes, err := ioutil.ReadFile(fileName)
-	message_list := &pb.MessageList{}
-	message_id := uuid.New().String()
+	log.Printf("\nRecieved: %v\n", in)
+	
+	createSql := `
+	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+	CREATE TABLE IF NOT EXISTS Messages(
+		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		user_id NUMERIC NOT NULL,
+		ts TIMESTAMP,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		deleted_at TIMESTAMP 
+	);
+	CREATE TABLE IF NOT EXISTS Message_data(
+		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		data TEXT,
+		content jsonb,
+		message_id UUID NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		deleted_at TIMESTAMP 
+	);`
+	
+	_, err := server.conn.Exec(context.Background(), createSql)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Table creation failed: %v", err)
+		os.Exit(1)
+	}
+	
 	created_message_data := &pb.MessageData{
-		Id: uuid.New().String(),
 		Data: in.GetMessageData().Data,
 		Content: in.GetMessageData().Content,
 	}
 	created_message := &pb.Message{
-		Id: message_id,
 		UserId: in.GetUserId(),
 		Ts: in.GetTs(),
 		MessageData: created_message_data,
 	}
+
+	//Create a transaction
+	tx, err := server.conn.Begin(context.Background())
 	if err != nil {
-		if os.IsNotExist(err) {
-			log.Print("File not found. Creating a new file")
-			message_list.Messages = append(message_list.Messages, created_message)
-			if errMsg, err := WriteToFile(message_list, fileName); err != nil {
-				log.Fatalf(errMsg, err)
-			}
-			return created_message, nil
-		}
-		log.Fatalf("Error reading file: %v", err)
+		log.Fatalf("conn.Begin failed: %v", err)
 	}
 
-	if err := protojson.Unmarshal(readBytes, message_list); err != nil {
-		log.Fatalf("Failed to pass message list: %v", err)
+	insertMessageSql := `insert into Messages(user_id, ts)
+	values($1, $2) returning id`
+
+	err = tx.QueryRow(
+		context.Background(), 
+		insertMessageSql, 
+		created_message.UserId, 
+		created_message.Ts.AsTime(),
+		).Scan(&created_message.Id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow Insert Message failed: %v\n", err)
+		os.Exit(1)
 	}
-	message_list.Messages = append(message_list.Messages, created_message)
-	if errMsg, err := WriteToFile(message_list, fileName); err != nil {
-		log.Fatalf(errMsg, err)
+
+	insertMessageDataSql := `insert into Message_data(data, content, message_id)
+	values($1, $2, $3) returning id`
+
+	err = tx.QueryRow(
+		context.Background(), 
+		insertMessageDataSql, 
+		created_message_data.Data, 
+		created_message.MessageData.Content, 
+		created_message.Id,
+	).Scan(&created_message_data.Id)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow Insert Message_data failed: %v\n", err)
+		os.Exit(1)
 	}
+
+	tx.Commit(context.Background())
 	return created_message, nil
 }
 
-func (s *UserMessageServer) GetMessages(
+func (server *UserMessageServer) GetMessages(
 	ctx context.Context, 
 	in *pb.GetMessageParams,
-	) (*pb.MessageList, error) {
-	jsonBytes, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Fatalf("Failed to read from file: %v", err)
-	}
+) (*pb.MessageList, error) {
 	message_list := &pb.MessageList{}
-	if err := protojson.Unmarshal(jsonBytes, message_list); err != nil {
-		log.Fatalf("Failed to Unmarshal: %v", err)
+	selectAllMessageSql := ""
+	var rows pgx.Rows
+	var err error
+	if in.StartTs != nil && in.EndTs != nil {
+		selectAllMessageSql = `select m.id, m.user_id, m.ts, md.id as data_id, md.data, md.content 
+		from Messages m 
+		left join Message_data md on md.message_id = m.id
+		where m.ts between $1 and $2`
+		rows, err = server.conn.Query(context.Background(), selectAllMessageSql, in.StartTs.AsTime(), in.EndTs.AsTime())
+		log.Printf("\nin.StartTs: %v, in.EndTs: %v", in.StartTs, in.EndTs)
+	} else {
+		selectAllMessageSql := `select m.id, m.user_id, m.ts, md.id as data_id, md.data, md.content 
+		from Messages m 
+		left join Message_data md on md.message_id = m.id`
+		rows, err = server.conn.Query(context.Background(), selectAllMessageSql)
 	}
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		message := &pb.Message{}
+		message_data := &pb.MessageData{}
+		var ts time.Time
+		err = rows.Scan(
+			&message.Id, 
+			&message.UserId,
+			&ts, 
+			&message_data.Id,
+			&message_data.Data,
+			&message_data.Content,
+		)
+		if err != nil {
+			return nil, err
+		}
+		message.MessageData = message_data
+		message.Ts = timestamppb.New(ts)
+		message_list.Messages = append(message_list.Messages, message)
+	}
+	
+	if rows.Err() != nil {
+    return nil, rows.Err()
+	}
+
 	return message_list, nil
 }
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	database_url := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+	log.Printf("database_url: %v", database_url)
+	conn, err := pgx.Connect(context.Background(), database_url)
+	if err != nil {
+		log.Fatalf("Unable to establish connection: %v", err)
+	}
+	defer conn.Close(context.Background())
 	user_msg_server := NewUserMessageServer()
+	user_msg_server.conn = conn
 	if err := user_msg_server.Run(); err != nil {
 		log.Fatalf("failed to run server: %v", err)
 	}
